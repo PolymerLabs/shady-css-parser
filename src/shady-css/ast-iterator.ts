a427daf479e59bd22fe11c39138c751f7cb121fc
Peter Burns <rictic@google.com>
2024-02-04 11:59:10 -0800

    Add getChildren and getNodesAtLocation APIs
    Also removed unnecessary util.inspect call, which makes it harder to use shady-css in the browser.

--- src/shady-css/ast-iterator.ts
+++ src/shady-css/ast-iterator.ts
@@ -9,41 +9,43 @@
  * rights grant found at http://polymer.github.io/PATENTS.txt
  */
 
-import * as util from 'util';
-
 import {Node, nodeType} from './common';
 
 export function* iterateOverAst(node: Node): Iterable<Node> {
   yield node;
+  for (const child of getChildren(node)) {
+    yield* iterateOverAst(child);
+  }
+}
+
+export function* getChildren(node: Node) {
   switch (node.type) {
     case nodeType.stylesheet:
-      for (const rule of node.rules) {
-        yield* iterateOverAst(rule);
-      }
+      yield* node.rules;
       return;
     case nodeType.ruleset:
-      return yield* iterateOverAst(node.rulelist);
+      yield node.rulelist;
+      return;
     case nodeType.rulelist:
-      for (const rule of node.rules) {
-        yield* iterateOverAst(rule);
-      }
+      yield* node.rules;
       return;
     case nodeType.declaration:
-      if (node.value !== undefined) {
-        yield* iterateOverAst(node.value);
+      if (node.value != null) {
+        yield node.value;
       }
       return;
     case nodeType.atRule:
-      if (node.rulelist) {
-        yield* iterateOverAst(node.rulelist);
+      if (node.rulelist != null) {
+        yield node.rulelist;
       }
       return;
     case nodeType.expression:
     case nodeType.comment:
     case nodeType.discarded:
-      return;  // no child nodes
+      return; // no child nodes
     default:
       const never: never = node;
-      console.error(`Got a node of unknown type: ${util.inspect(never)}`);
+      console.error(`Got a node of unknown type: ${never}`);
   }
 }
+
